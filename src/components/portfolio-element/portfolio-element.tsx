25dc485d01f24a2e13766a8fe7d321fdf7650550
AnyKeyGames <pressanykeygames@gmail.com>
2022-10-29 17:22:34 +0100

    working projects

--- src/components/portfolio-element/portfolio-element.tsx
+++ src/components/portfolio-element/portfolio-element.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import { PortfolioModal } from '../portfolio-modal';
 
 export function PortfolioElement(props: any): JSX.Element {
   const delay = { "--animation-delay": props.number } as React.CSSProperties;
+
+    useEffect(() => {
+      const hiddenElements = document.querySelectorAll('.hidden');
+      const observer = new IntersectionObserver((entries) => {
+              entries.forEach((entry) => {
+                  if(entry.isIntersecting){
+                      entry.target.classList.add('show');
+                  } else {
+                      entry.target.classList.remove('show');
+                  }
+              })
+          })
+      hiddenElements.forEach((el) => observer.observe(el))
+    })
 
   return (
     <>
